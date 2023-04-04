99fd7b53bb2bab165d9a7070ec651fbe889eb7b9
Robert Małafiejew <01169593@pw.edu.pl>
2023-04-04 18:35:10 +0200

    File calculator.fxml replaced with an alternative, which renamed to calculator.fxml; quickfixes in App; added action in Controller

--- src/main/java/pl/edu/pw/ee/App.java
+++ src/main/java/pl/edu/pw/ee/App.java
@@ -1,12 +1,11 @@
 package pl.edu.pw.ee;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
 
@@ -14,22 +13,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("calculator2.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("calaulator2.fxml"));
         scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("Advanced Calculator");
-        stage.setResizable(false); 
+        stage.setResizable(false);
         stage.show();
     }
-
-    /*static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml));
-        return fxmlLoader.load();
-    }*/
 
     public static void main(String[] args) {
         launch();
--- src/main/java/pl/edu/pw/ee/Controller.java
+++ src/main/java/pl/edu/pw/ee/Controller.java
@@ -1,8 +1,17 @@
 package pl.edu.pw.ee;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
-    
+    //
+
+    @FXML
+    private void clickTextBtn(ActionEvent event) {
+        Button buttonClicked = (Button) event.getSource(); 
+        System.out.println(buttonClicked.getText());
+    }
 }
