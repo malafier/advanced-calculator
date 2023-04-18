90a3f9459674240bcfc2d9ef61e88222a6a92352
Robert Małafiejew <01169593@pw.edu.pl>
2023-04-18 18:39:31 +0200

    added some logic to Calculations added ShuttingYard algorythm parser and utility parsers

--- src/main/java/pl/edu/pw/ee/calculations/Calculations.java
+++ src/main/java/pl/edu/pw/ee/calculations/Calculations.java
@@ -1,15 +1,78 @@
 package pl.edu.pw.ee.calculations;
 
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
 public class Calculations {
-    public Calculations(String input) {
+    public Calculations(String input) throws Exception {
         if(!validateInput(input)) {
-            // TODO:implement exception
+            throw new Exception(); //TODO: implement exception
         }
         //TODO: implement
     }
 
+    private Stack<String> shuntingYardParser(String input) {
+        StringTokenizer tokens = new StringTokenizer(input, "+-×÷()^", true);
+        Stack<String> operatorStack = new Stack<String>();
+        Stack<String> outputStack = new Stack<String>(); 
+
+        Map<String, Integer> operatorPrecedence = new TreeMap<>(); 
+        operatorPrecedence.put("^", 4);
+        operatorPrecedence.put("×", 3);
+        operatorPrecedence.put("÷", 3);
+        operatorPrecedence.put("+", 2);
+        operatorPrecedence.put("-", 2);
+
+        while(tokens.hasMoreTokens()) {
+            String token = tokens.nextToken();
+
+            if(isNumber(token)) {
+                outputStack.push(token); 
+            } else {
+                if(token.equals("(")) {
+                    while(operatorStack.peek().equals(")")) {
+                        outputStack.push(operatorStack.pop()); 
+                    }
+                } else if(token.equals(")")) {
+                    operatorStack.push(token);
+                } else if(operatorStack.empty() || operatorStack.peek().equals("(") || operatorPrecedence.get(operatorStack.peek()) < operatorPrecedence.get(token)) {
+                    operatorStack.push(token);
+                } else {
+                    outputStack.push(operatorStack.pop()); 
+                    operatorStack.push(token); 
+                }
+            }
+        }
+
+        while(!operatorStack.empty()) {
+            outputStack.push(operatorStack.pop());
+        }
+
+        return outputStack;
+    }
+
+    private ComplexNum calculate(Stack<String> rpn) {
+        rpn = reverseStack(rpn);
+
+        return null; //TODO: implement
+    }
+
     public String getAnswer() {
         return null; //TODO: implement
+    }
+
+    private boolean isNumber(String str) {
+        return str.matches("[0-9.i]+");
+    }
+
+    private static Stack<String> reverseStack(Stack<String> stack) {
+        Stack<String> reversed = new Stack<String>(); 
+        while(!stack.empty()) {
+            reversed.push(stack.pop()); 
+        }
+        return reversed;
     }
 
     private boolean validateInput(String input) {
