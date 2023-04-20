a82ab802e42397e00fe3e6e5a61e21c4889010fd
Robert Małafiejew <01169593@pw.edu.pl>
2023-04-20 20:15:33 +0200

    first finish of logic finished logic part; not testet yet

--- src/main/java/pl/edu/pw/ee/calculations/Calculations.java
+++ src/main/java/pl/edu/pw/ee/calculations/Calculations.java
@@ -6,14 +6,16 @@ import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Calculations {
-    public Calculations(String input) throws Exception {
+
+    public static String getAnswer(String input) throws IllegalArgumentException, ArithmeticException {
         if(!validateInput(input)) {
-            throw new Exception(); //TODO: implement exception
+            throw new IllegalArgumentException();
         }
-        //TODO: implement
+        Stack<String> reversePolish = shuntingYardParser(input);
+        return calculate(reversePolish).toString();
     }
 
-    private Stack<String> shuntingYardParser(String input) {
+    private static Stack<String> shuntingYardParser(String input) {
         StringTokenizer tokens = new StringTokenizer(input, "+-×÷()^", true);
         Stack<String> operatorStack = new Stack<String>();
         Stack<String> outputStack = new Stack<String>(); 
@@ -53,17 +55,25 @@ public class Calculations {
         return outputStack;
     }
 
-    private ComplexNum calculate(Stack<String> rpn) {
+    private static ComplexNum calculate(Stack<String> rpn) throws ArithmeticException {
         rpn = reverseStack(rpn);
+        Stack<ComplexNum> helpingStack = new Stack<>(); 
 
-        return null; //TODO: implement
+        while(!rpn.empty()) {
+            String popped = rpn.pop(); 
+            if(isNumber(popped)) {
+                helpingStack.push(new ComplexNum(popped)); 
+            } else {
+                ComplexNum second = helpingStack.pop(); 
+                ComplexNum first = helpingStack.empty() ? new ComplexNum(0, 0) : helpingStack.pop(); 
+                helpingStack.push(first.performOperation(second, popped));
+            }
+        }
+
+        return helpingStack.pop();
     }
 
-    public String getAnswer() {
-        return null; //TODO: implement
-    }
-
-    private boolean isNumber(String str) {
+    private static boolean isNumber(String str) {
         return str.matches("[0-9.i]+");
     }
 
@@ -75,7 +85,7 @@ public class Calculations {
         return reversed;
     }
 
-    private boolean validateInput(String input) {
+    private static boolean validateInput(String input) {
         int leftPar = input.chars().filter(c -> c == '(').sum();
         int rightPar = input.chars().filter(c -> c == ')').sum();
 
@@ -87,13 +97,6 @@ public class Calculations {
                 sb.append(')');
             }
             input = sb.toString();
-        }
-
-        String operands = "+-×÷."; 
-        for(int i=1; i < input.length()-1; i++) {
-            if(operands.contains(Character.toString(input.charAt(i))) && (operands.contains(Character.toString(input.charAt(i-1))) || operands.contains(Character.toString(input.charAt(i+1))))) {
-                return false; 
-            }
         }
 
         return true;
--- src/main/java/pl/edu/pw/ee/calculations/ComplexNum.java
+++ src/main/java/pl/edu/pw/ee/calculations/ComplexNum.java
@@ -1,6 +1,6 @@
 package pl.edu.pw.ee.calculations;
 
-public class ComplexNum extends Number { // extends Number
+public class ComplexNum {
     private double real; 
     private double imaginary; 
 
@@ -10,7 +10,30 @@ public class ComplexNum extends Number { // extends Number
     }
 
     public ComplexNum(String s) {
-        // TODO: zaimplementować, xd
+        if(s.contains("i")) {
+            real = Double.parseDouble(s); 
+            imaginary = 0;
+        } else {
+            real = 0;
+            imaginary = Double.parseDouble(s.substring(0, s.lastIndexOf(s)-1));
+        }
+    }
+
+    public ComplexNum performOperation(ComplexNum other, String operator) throws ArithmeticException {
+        switch (operator) {
+            case "+":
+                return this.add(other);
+            case "-":
+                return this.substract(other);
+            case "×":
+                return this.multiply(other);
+            case "÷":
+                return this.divide(other);
+            case "^":
+                return this.powerOf(other);
+            default:
+                throw new ArithmeticException();
+        }
     }
 
     public ComplexNum add(ComplexNum other) {
@@ -34,9 +57,9 @@ public class ComplexNum extends Number { // extends Number
         return new ComplexNum(resultReal, resultImaginary); 
     }
 
-    public ComplexNum divide(ComplexNum other) {
+    public ComplexNum divide(ComplexNum other) throws ArithmeticException {
         if(other.real == 0 && other.imaginary == 0) {
-            throw new IllegalArgumentException("You can't divide by ZERO!"); 
+            throw new ArithmeticException("You can't divide by ZERO!"); 
         }
 
         double otherLengthSquared = other.real * other.real + other.imaginary * other.imaginary; 
@@ -47,32 +70,21 @@ public class ComplexNum extends Number { // extends Number
         return new ComplexNum(resultReal, resultImaginary); 
     }
 
+    public ComplexNum powerOf(ComplexNum other) throws ArithmeticException {
+        if(other.imaginary != 0 || other.real % 1 != 0) {//add rooting real numbers
+            throw new ArithmeticException();
+        }
+
+        ComplexNum answer = new ComplexNum(1, 0); 
+        for(int i=1; i < other.real; i++) {
+            answer.multiply(this); 
+        }
+
+        return answer;
+    }
+
     @Override
     public String toString() {
-        return real + " " + imaginary + "i"; 
-    }
-
-    @Override
-    public int intValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'intValue'");
-    }
-
-    @Override
-    public long longValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'longValue'");
-    }
-
-    @Override
-    public float floatValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'floatValue'");
-    }
-
-    @Override
-    public double doubleValue() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doubleValue'");
+        return real + "+" + imaginary + "i"; 
     }
 }
